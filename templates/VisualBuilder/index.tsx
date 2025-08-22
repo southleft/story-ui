1816b72e62e830602bc58f909f34755e8d21a81b
TJ Pitre <tpitre@southleft.com>
2025-08-21 23:37:58 -0500

    Revert "feat(cli): install Visual Builder MVP and deps during init"
    This reverts commit 246cde2d89ddd4fe816daacbb03c05763dc79cb2.

--- templates/VisualBuilder/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as VisualBuilder } from './VisualBuilder';
--- templates/VisualBuilder/types.ts
+++
@@ -1,9 +0,0 @@
-export type ComponentTypeName = 'Text' | 'Button' | 'Group' | 'Stack' | 'Card';
-
-export interface ComponentNode {
-  id: string;
-  type: ComponentTypeName;
-  props: Record<string, any>;
-  children: string[];
-  parentId?: string;
-}
